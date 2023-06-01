1fbe4803a2ccf075e69e651cf6d997edd4ffa1cd
August <117049354+Auggie0w0@users.noreply.github.com>
2023-06-01 16:09:01 -0700

    iteration practices

---
+++ Rock Paper Scissors/RockPaperScissors/src/iterations.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class iterations {
+    public static void main(Stringp[] args) {
+        int hotdogCount;
+        double hotdogPrice = 2.50;
+        int drinkCount;
+        double drinkPrice = 1.25;
+        double totalCost = 0;
+        double payment = 0;
+    }
+}
