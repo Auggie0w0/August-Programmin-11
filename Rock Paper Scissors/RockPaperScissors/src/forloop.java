a08b9060d2acc1ef57907e0ce6b0c8d73d280337
August <117049354+Auggie0w0@users.noreply.github.com>
2023-06-01 21:01:38 -0700

    exercises and learning

---
+++ Rock Paper Scissors/RockPaperScissors/src/forloop.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class forloop {
+    public static void main(String[] args) {
+        String name = "John Jones";
+        
+    }
+}
--- Rock Paper Scissors/RockPaperScissors/src/iterations.java
+++ Rock Paper Scissors/RockPaperScissors/src/iterations.java
@@ -1,11 +1,36 @@
 import java.util.*;
 public class iterations {
-    public static void main(Stringp[] args) {
-        int hotdogCount;
+    public static void main(String[] args) {
+        int hotdogCount = 0;
         double hotdogPrice = 2.50;
-        int drinkCount;
+        int drinkCount = 0;
         double drinkPrice = 1.25;
         double totalCost = 0;
         double payment = 0;
+        Scanner scan = new Scanner(System.in);
+
+        do  {
+            System.out.println("How many hotdogs would you like to buy");
+            hotdogCount = Integer.parseInt(scan.next());
+        } while (hotdogCount < 0);
+
+        totalCost += hotdogCount*hotdogPrice;
+
+        do {
+            System.out.println("How many drinks would you like to buy?");
+            drinkCount = Integer.parseInt(scan.next());
+        } while (drinkCount < 0);
+
+        totalCost += drinkPrice*drinkCount;
+        System.out.println("You have to pay " + totalCost);
+        System.out.println("How would you like to pay?");
+        payment = Double.parseDouble(scan.next());
+        if(payment > totalCost) {
+            System.out.println("Your change is " + (payment - totalCost));
+        } else if (payment == totalCost) {
+            System.out.println("Exact change way to go");
+        } else {
+            System.out.println("Not enough money you owe us " + (totalCost - payment));
+        }
     }
 }
