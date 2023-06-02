ead6c94db623d9c4576fa042fdfb49049b5f1398
August <117049354+Auggie0w0@users.noreply.github.com>
2023-06-01 21:28:56 -0700

    finishing the forloop exercise/ practice

--- Rock Paper Scissors/RockPaperScissors/src/forloop.java
+++ Rock Paper Scissors/RockPaperScissors/src/forloop.java
@@ -2,6 +2,10 @@ import java.util.*;
 public class forloop {
     public static void main(String[] args) {
         String name = "John Jones";
-        
+        for(int i=0; i<name.length(); i++) {
+            if(name.substring(i,i+1).equals("J")) {
+                System.out.println("index position: " + i + "\t" + name.substring(i, i+1));
+            }
+        }
     }
 }
