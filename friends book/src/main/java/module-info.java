29f30c6ccc30600b8fb6384c54f8bbfd3f713098
August <117049354+Auggie0w0@users.noreply.github.com>
2024-02-26 21:36:04 -0800

    friends book finished

--- friends book/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
-}
